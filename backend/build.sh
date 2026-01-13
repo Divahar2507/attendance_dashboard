384930bf10700f5a718bfb523ed6d449d5675001
Divahar2507 <divaharp2004@gmail.com>
2026-01-13 15:43:14 +0530

    Auto-create superuser during build

--- backend/build.sh
+++ backend/build.sh
@@ -6,3 +6,15 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# Create superuser automatically if variables are set
+if [ "$DJANGO_SUPERUSER_USERNAME" ]; then
+    python -c "from api.models import User; import os; \
+    username=os.environ.get('DJANGO_SUPERUSER_USERNAME'); \
+    email=os.environ.get('DJANGO_SUPERUSER_EMAIL'); \
+    password=os.environ.get('DJANGO_SUPERUSER_PASSWORD'); \
+    User.objects.filter(username=username).exists() or \
+    User.objects.create_superuser(username, email, password)"
+fi
+
+
